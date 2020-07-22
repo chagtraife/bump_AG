289b8a45a2ef1660a3f25882dd7590a7718f0e55
chagtraife <thang.nguyenngoc1996@gmail.com>
2020-07-22 23:34:20 +0700

    edit q message

--- AG/AG/console_enhance.cpp
+++ AG/AG/console_enhance.cpp
@@ -1,5 +1,6 @@
 #include "console_enhance.h"
 #include "ui_console_enhance.h"
+#include <QMessageBox>
 
 Console_Enhance::Console_Enhance(QWidget *parent) :
     QDialog(parent),
@@ -22,8 +23,13 @@ void Console_Enhance::on_btnOk_clicked()
     From = ui->txtFrom->text().trimmed().toInt();
     To = ui->txtTo->text().trimmed().toInt();
     offset = ui->txtOffset->text().trimmed().toInt();
+    if (To>From){
     this->hide();
     emit into_enhance();
+    }else {
+QMessageBox::information(this, "Warning", "\"From\" value must be greater than \"To\" value  ");
+}
+
 }
 
 void Console_Enhance::closeEvent (QCloseEvent *event){
